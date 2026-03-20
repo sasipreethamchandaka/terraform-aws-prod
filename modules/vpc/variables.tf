fb218df92df57d63d082f453b01dd170be315f6e
sasi preetham chandaka <87517348+sasipreethamchandaka@users.noreply.github.com>
2026-03-20 11:15:41 +0530

    Initial Terraform AWS infra setup

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "cidr_block" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+  default     = 2
+}
