24a432267ca057bcf248af6876934a02195a46f7
sasi preetham chandaka <87517348+sasipreethamchandaka@users.noreply.github.com>
2026-03-20 11:12:12 +0530

    Initial Terraform AWS infra setup

---
+++ modules/rds/main.tf
@@ -0,0 +1,9 @@
+resource "aws_db_instance" "db" {
+  allocated_storage = 20
+  engine            = "mysql"
+  instance_class    = "db.t3.micro"
+  username          = "admin"
+  password          = "admin123"
+
+  skip_final_snapshot = true
+}
